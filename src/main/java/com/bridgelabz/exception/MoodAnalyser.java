4d42220cda069dc930e47b67822e101345bdba5f
spoo0143 <gowdaspoorthi28@gmail.com>
2021-11-02 19:10:41 +0530

    added:pom file added

---
+++ src/main/java/com/bridgelabz/exception/MoodAnalyser.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.exception;
+
+	/**
+	 * Purpose - “I am in Sad Mood” message Should Return SAD
+	 *
+	 * 
+	 */
+
+	public class MoodAnalyser {
+		public static String analyseMood(String message) {
+			return "Sad";
+		}
+
+	}
